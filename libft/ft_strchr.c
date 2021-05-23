87cea7646cac130c071d09962ea256d35a814bf5
vinihora <vinidahora08@hotmail.com>
2021-05-23 02:37:48 -0300

    23/05 - 02:37

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: veduardo <veduardo@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 10:52:59 by veduardo          #+#    #+#             */
-/*   Updated: 2021/05/22 15:34:21 by veduardo         ###   ########.fr       */
+/*   Updated: 2021/05/23 02:32:17 by veduardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
-		if ((char)c == *s)
+		if (*s == (char)c)
 		{
 			return ((char *)s);
 		}
 		s++;
 	}
+	if (c == 0)
+		return ((char *)s);
 	return (NULL);
 }
