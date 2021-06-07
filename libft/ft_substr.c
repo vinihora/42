4d73953437e38d598575c177e17965cacf193301
vinihora <vinidahora08@hotmail.com>
2021-06-07 18:48:35 -0300

    07/06

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: veduardo <veduardo@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 15:50:25 by veduardo          #+#    #+#             */
-/*   Updated: 2021/06/07 18:47:47 by veduardo         ###   ########.fr       */
+/*   Updated: 2021/06/07 18:48:25 by veduardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*ret;
 	unsigned int	count;
 
-	ret = ft_calloc((len) * sizeof(s));
+	ret = ft_calloc((len), sizeof(s));
 	if (!(ret))
 		return (NULL);
 	i = 0;
