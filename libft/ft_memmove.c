09a78b435903b1efc59da03d6fd7db220a684ea5
vinihora <vinidahora08@hotmail.com>
2021-06-07 18:55:35 -0300

    07/06

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: veduardo <veduardo@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 11:58:11 by veduardo          #+#    #+#             */
-/*   Updated: 2021/06/07 18:54:54 by veduardo         ###   ########.fr       */
+/*   Updated: 2021/06/07 18:55:32 by veduardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	s = (char *)src;
 	d = (char *)dst;
 	i = 0;
-	if (!(len))
+	if (!(dst))
 		return (NULL);
 	if (d > s)
 		while (len-- > 0)
