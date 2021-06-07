e9601e6170afb661144138ce5d52fb18ebc7ec4f
vinihora <vinidahora08@hotmail.com>
2021-06-07 20:04:45 -0300

    07/06/2021

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: veduardo <veduardo@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 17:00:23 by veduardo          #+#    #+#             */
-/*   Updated: 2021/06/07 20:03:05 by veduardo         ###   ########.fr       */
+/*   Updated: 2021/06/07 20:04:30 by veduardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!(s1) && !(s2))
 		return (NULL);
-	i[2] = 0;
+	i[0] = 0;
 	i[1] = 0;
 	res = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
 	if (!(res))
 		return (NULL);
-	while (s1[i[2]])
+	while (s1[i[0]])
 	{
-		res[i[1]] = s1[i[2]];
+		res[i[1]] = s1[i[0]];
 		i[1]++;
-		i[2]++;
+		i[0]++;
 	}
-	i[2] = 0;
-	while (s2[i[2]])
+	i[0] = 0;
+	while (s2[i[0]])
 	{
-		res[i[1]] = s2[count];
+		res[i[1]] = s2[i[0]];
 		i[1]++;
-		i[2]++;
+		i[0]++;
 	}
 	return (res);
 }
